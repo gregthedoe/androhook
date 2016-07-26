55e35eb292117c1d392454092b1b6775b1465227
Greg Doe <gregthedoe@gmail.com>
2016-07-26 14:06:07 +0300

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='androhook',
-    version='0.1',
-    packages=['androhook', 'androhook.network', 'androhook.network.flow_writers'],
-    url='https://github.com/gregthedoe/androhook',
-    license='MIT',
-    author='Greg',
-    author_email='',
-    description='A frida based hooking framework for android devices used mainly for app research',
-    scripts=['scripts/intercept_ssl.py']
+        name='androhook',
+        version='0.1',
+        url='https://github.com/gregthedoe/androhook',
+        license='MIT',
+        author='Greg',
+        author_email='',
+        description='A frida based hooking framework for android devices used mainly for app research',
+        packages=['androhook', 'androhook.network', 'androhook.network.flow_writers'],
+        include_package_data=True,
+        scripts=['scripts/intercept_ssl.py'],
+        install_requires=[
+            'frida',
+            'mitmproxy>=0.17',
+            'datetime'
+        ]
 )
