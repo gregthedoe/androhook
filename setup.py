1dfd052aa4c7b70910f8df028e0b2bea2ce118cc
Greg Doe <gregthedoe@gmail.com>
2016-07-26 12:40:59 +0300

    Add intercept_ssl script to setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(
-        name='androhook',
-        version='0.1',
-        packages=['androhook', 'androhook.network', 'androhook.network.flow_writers'],
-        url='https://github.com/gregthedoe/androhook',
-        license='MIT',
-        author='greg',
-        author_email='',
-        description='A frida based hooking framework for android devices used mainly for app research'
+    name='androhook',
+    version='0.1',
+    packages=['androhook', 'androhook.network', 'androhook.network.flow_writers'],
+    url='https://github.com/gregthedoe/androhook',
+    license='MIT',
+    author='Greg',
+    author_email='',
+    description='A frida based hooking framework for android devices used mainly for app research',
+    scripts=['scripts/intercept_ssl.py']
 )
